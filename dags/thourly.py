5efc70b5cf448b153d1d0a2de48ce5fecae2bd6c
Tomasz Bartczak <tomasz.bartczak@allegrogroup.com>
2016-02-11 09:05:01 +0100

    simplified

--- dags/thourly.py
+++ dags/thourly.py
@@ -3,42 +3,29 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators import BashOperator, DummyOperator
 
-dag = DAG("NDCG_HOUR1",
+dag = DAG("MYDAG",
           default_args={"owner" : "me",
-                        "start_date":datetime.now() - timedelta(minutes=10)},
-          schedule_interval='* * * * *',
+                        "start_date":datetime.now() - timedelta(minutes=100)},
+          schedule_interval='*/3 * * * *',
           concurrency=6,
           dagrun_timeout=timedelta(minutes=4)
           )
 
-airflow_logs = "/tmp/scheduler-modelling.log"
-default_params = {
-    'airflow_logs' : airflow_logs
-}
-
-def ndcg_daily_command():
-    return ("/usr/local/airflow/dags/echo.sh spark_ndcg " +
-            "{{ (execution_date - macros.timedelta(hours = 1)).strftime('%Y/%m/%d %H') }} " +
-            " 2>&1 >> %s" % airflow_logs)
-
-
-def ndcg_add_hive_partition_command():
-    return ("/usr/local/airflow/dags/echo.sh hive_ndcg " +
-            "{{ (execution_date - macros.timedelta(hours = 1)).strftime('%Y %m %d %H') }} " +
-            " 2>&1 >> %s" % airflow_logs)
-
-run_id = datetime.now().strftime("%Y_%m_%d_%H")
+def command():
+    return "echo {{ params.myparam }} {{ (execution_date - macros.timedelta(hours = 1)).strftime('%Y/%m/%d %H %M') }} 2>&1 >> /tmp/logs"
 
 run_this_last = DummyOperator(task_id='run_this_last', dag=dag)
 
-ndcgHourly = BashOperator(
-        task_id='spark_submit_ndcg_' + str(run_id),
-        bash_command=ndcg_daily_command(),
+firstTask = BashOperator(
+        task_id='first',
+        bash_command=command(),
+        params= {"myparam":"1"},
         dag=dag)
-ndcgAddHive = BashOperator(
-        task_id='hive_partition_ndcg_' + str(run_id),
-        bash_command=ndcg_add_hive_partition_command(),
+secondTask = BashOperator(
+        task_id='second',
+        bash_command=command(),
+        params= {"myparam":"2"},
         dag=dag)
 
-ndcgHourly.set_downstream(ndcgAddHive)
-ndcgAddHive.set_downstream(run_this_last)
+firstTask.set_downstream(secondTask)
+secondTask.set_downstream(run_this_last)
